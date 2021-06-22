bfeb084a22e01ca75d918cb88f315fbd6c59b938
AlinaDev252 <costachealina25@gmail.com>
2021-06-22 13:16:05 +0300

    Add the components and data method to the project

---
+++ dynamic-components/src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+	<h1>Home Page</h1>
+</template>
+
+<script>
+export default {
+	unmounted() {
+		console.log("Home component was unmounted!");
+	},
+	activated() {
+		console.log("Home component activated");
+	},
+	deactivated() {
+		console.log("Home component deactivated");
+	},
+};
+</script>
